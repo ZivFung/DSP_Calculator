bb356e79e071caeb5c054da97bb9234ae9727a68
Jiaxiang Feng <JiaxiangFeng0612@outlook.com>
2019-12-09 15:27:22 +0800

    add phase spectrum

--- ImageSpectrum.m
+++ ImageSpectrum.m
@@ -1,10 +1,12 @@
-function [spectrum] = ImageSpectrum(img)
+function [spectrum, phase] = ImageSpectrum(img)
 % This function return the spectrum of image, the depth of input image must
 % be 1.
     XNum = size(img,2);
     YNum = size(img,1);
-    img=double(img);
-    freq=fft2(img);
-    magnitude=log(1+abs(freq));
-    spectrum = magnitude(1:ceil(YNum/2), 1:ceil(XNum/2));
+    img = double(img);
+    freq = fft2(img);
+    magnitude = log(1+abs(freq));
+    phaseImg = (angle(freq)/pi);
+    spectrum = magnitude(1:floor(YNum/2)+1, 1:floor(XNum/2)+1);
+    phase = phaseImg(1:floor(YNum/2)+1, 1:floor(XNum/2)+1);
 end
